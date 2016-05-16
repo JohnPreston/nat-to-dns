ef168e8eccefd0a0723affb7883222d2722f978a
John Mille <john@ews-network.net>
2016-05-16 03:12:58 -0400

    A bit of cleanup

--- ansible-play/roles/haproxyconfig/scripts/ansible_script.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-for i in {1..4}; do
-    ansible-playbook /var/tmp/MyELBtoDNS/ansible-play/elb.yml -e elb_dns_name=`head -1 /var/tmp/elb.conf` -e ec2_url=`head -1 /var/tmp/ec2.conf`  -e s3_url=`head -1 /var/tmp/s3.conf`
-    sleep 14
-done
